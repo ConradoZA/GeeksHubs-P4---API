62a5f16c086788efc64a5051ea641d3a77864225
Conrado <zaolen@gmail.com>
2020-03-19 18:20:15 +0100

    update refined

--- backend/controllers/GameController.js
+++ backend/controllers/GameController.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { Game, Type, Mechanic, sequelize } = require('../models/index.js')
+const { Game, Type, Mechanic, Author, Artist, sequelize } = require('../models/index.js')
 
 const GameController = {
     getAll(req, res) {
-        Game.findAll({ include: [Type, Mechanic] })
+        Game.findAll({ include: [Type, Mechanic, Author, Artist] })
             .then(games => res.send(games))
             .catch(err => res.status(500).send('Ha habido problemas al tratar de obtener los juegos.'))
     },
     getOne(req, res) {
         Game.findOne({
-                include: [Type, Mechanic],
+                include: [Type, Mechanic, Author, Artist],
                 where: { id: req.params.id }
             })
             .then(game => res.send(game))
@@ -45,14 +45,22 @@ const GameController = {
         Game.update({...req.body }, { where: { id: req.params.id } })
             .then(game => Game.findByPk(req.params.id))
             .then(game => {
-                sequelize.query(`DELETE FROM TypeGames where GameId = ${game.id}`);
-                sequelize.query(`DELETE FROM MechanicGames where GameId = ${game.id}`);
-                sequelize.query(`DELETE FROM AuthorGames where GameId = ${game.id}`);
-                sequelize.query(`DELETE FROM ArtistGames where GameId = ${game.id}`);
-                game.addType(req.body.TypeId);
-                game.addMechanic(req.body.MechanicId);
-                game.addAuthor(req.body.AuthorId);
-                game.addArtist(req.body.ArtistId);
+                if (req.body.TypeId) {
+                    sequelize.query(`DELETE FROM TypeGames where GameId = ${game.id}`);
+                    game.addType(req.body.TypeId);
+                }
+                if (req.body.MechanicId) {
+                    sequelize.query(`DELETE FROM MechanicGames where GameId = ${game.id}`);
+                    game.addMechanic(req.body.MechanicId);
+                }
+                if (req.body.AuthorId) {
+                    sequelize.query(`DELETE FROM AuthorGames where GameId = ${game.id}`);
+                    game.addAuthor(req.body.AuthorId);
+                }
+                if (req.body.ArtistId) {
+                    sequelize.query(`DELETE FROM ArtistGames where GameId = ${game.id}`);
+                    game.addArtist(req.body.ArtistId);
+                }
                 res.status(200).send(game)
             })
             .catch(err => res.status(500).send('Ha habido problemas al tratar de actualizar el juego.'))
